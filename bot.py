dc3df091b5b3d40229ee9604394206381c613fd4
SQ <shaoqin.lu@gmail.com>
2023-02-23 22:54:01 +0800

    disable help handler for now

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 #Message displayed when user sends an unrecognised command to the bot
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Please use /config to change your settings!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, I'm Frank :)")
 
 #Sends content to the chat
 async def send_prompt(context: ContextTypes.DEFAULT_TYPE):
@@ -92,7 +92,7 @@ if __name__ == '__main__':
     help_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), help)
 
     application.add_handler(start_handler)
-    application.add_handler(help_handler)
+    #application.add_handler(help_handler) - disable this for now
     application.add_handler(card_handler)
     
     application.run_polling()
